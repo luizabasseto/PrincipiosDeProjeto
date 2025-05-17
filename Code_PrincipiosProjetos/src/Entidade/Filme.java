812c9bcd23878862acccdef10a41e5b7899746c6
Luiza Batista Basseto <luizabasseto.1@gmail.com>
2025-05-17 09:09:24 -0300

    Update Filme.java

--- Code_PrincipiosProjetos/src/Entidade/Filme.java
+++ Code_PrincipiosProjetos/src/Entidade/Filme.java
@@ -18,24 +18,6 @@ public class Filme {
 
     public String getNome_filme() {
         return nome_filme;
-    }
-
-    public void setNome_filme(String nome_filme) {
-        this.nome_filme = nome_filme;
-    }
-
-    public int getId_filme() {
-        return id_filme;
-    }
-
-    public void setId_filme(int id_filme) {
-        this.id_filme = id_filme;
-    }
-
-    @Override
-    public String toString() {
-        return id_filme + ";" + nome_filme;
-    }
-    
+    }    
     
 }
